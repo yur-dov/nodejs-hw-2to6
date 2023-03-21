087bd6bb0e97b2d993a5cf45d641c47b01b1192d
Yurii <Rapacta@gmail.com>
2023-03-20 22:41:22 -0600

    for-check

--- middleware/index.js
+++ middleware/index.js
@@ -4,7 +4,6 @@ const isValidId = require('./isValidId');
 const authenticate = require('./authenticate');
 const upload = require('./upload');
 
-
 module.exports = {
     validateBody,
     handleSaveError,
